30bc0fc83649bdacbbe172528ac7a4b2feaf657c
Nicholas <148683602+itzzNicholas@users.noreply.github.com>
2023-12-10 17:40:12 +0100

    Uploaded XMPP Web Server into astraChats

---
+++ local.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-unused-vars, no-var
+var config = {
+  name: 'XMPP web',
+  transports: {
+    websocket: 'wss://chat.domain-web.ltd/xmpp-websocket',
+  },
+  hasGuestAccess: true,
+  hasRegisteredAccess: true,
+  anonymousHost: null,
+  // anonymousHost: 'anon.domain-xmpp.ltd',
+  isTransportsUserAllowed: false,
+  hasHttpAutoDiscovery: false,
+  resource: 'Web XMPP',
+  defaultDomain: 'domain-xmpp.ltd',
+  defaultMuc: null,
+  // defaultMuc: 'conference.domain-xmpp.ltd',
+  isStylingDisabled: false,
+  hasSendingEnterKey: false,
+  connectTimeout: 5000,
+  pinnedMucs: [],
+  logoUrl: '',
+  guestDescription: '',
+}
